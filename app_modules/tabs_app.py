4a9e965951db7c21eb730510b54682093a766cd4
Angel Ragas <angelragas@gmail.com>
2025-03-16 14:31:08 -0400

    Update tabs_app.py
    In order to run the app

--- app_modules/tabs_app.py
+++ app_modules/tabs_app.py
@@ -16,3 +16,21 @@ def citibike_poverty_tab(poverty_df, geojson_data, zip_code_station_count, pover
     with tab3:
         st.header("Poverty vs Number of Trips")
         st.plotly_chart(plot_poverty_scatter(poverty_trips_df))
+
+def show_initial_proposal():
+    with open("app_documentation/initial_proposal.md", "r", encoding="utf-8") as file:
+        initial_content = file.read()
+    st.markdown("""
+    # 📌 Initial Proposal
+    ---
+    """, unsafe_allow_html=True)
+    st.markdown(initial_content, unsafe_allow_html=True)
+
+def show_revisited_proposal():
+    with open("app_documentation/revisited_proposal.md", "r", encoding="utf-8") as file:
+        revisited_content = file.read()
+    st.markdown("""
+    # 🔍 Revisited Proposal
+    ---
+    """, unsafe_allow_html=True)
+    st.markdown(revisited_content, unsafe_allow_html=True)
